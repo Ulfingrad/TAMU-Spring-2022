4022c33466ddfb5a2abf79c6878e66b168d6aac8
Ulfingrad <austinslobodnik@gmail.com>
2022-02-02 13:53:15 -0600

    initialCommit

---
+++ packages/api/src/env.ts
@@ -0,0 +1,16 @@
+/* istanbul ignore file */
+import { config } from 'dotenv-flow';
+import setEnv from '@americanairlines/simple-env';
+
+config();
+
+export const env = setEnv({
+  required: {
+    nodeEnv: 'NODE_ENV',
+    port: 'PORT',
+    databaseUrl: 'DATABASE_URL',
+  },
+  optional: {
+    databaseUser: 'DATABASE_USER',
+  },
+});
