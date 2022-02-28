b9d831a8e54330e218152d68bbc3ab79507f2847
gageayk <64162749+gageayk@users.noreply.github.com>
2022-02-28 14:51:56 -0600

    created migration for destinations table

---
+++ packages/api/src/migrations/0000-add-the-destinations-table.ts
@@ -0,0 +1,12 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20210927234531 extends Migration {
+  async up(): Promise<void> {
+    // this.addSql('create table "user" ("id" bigserial primary key, "createdAt" timestamptz(0) not null default clock_timestamp(), "updatedAt" timestamptz(0) not null default clock_timestamp(), "name" text not null);');
+    this.addSql('CREATE TABLE "Destinations" ("id" bigserial primary key, "orig" text not null, "dest" text not null, "period" text not null);');
+  }
+
+  async down(): Promise<void> {
+    this.addSql('drop table "Destinations";');
+  }
+}
