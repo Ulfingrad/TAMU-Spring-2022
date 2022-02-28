3ecf728558f81b555623caac59f4d2b9584eedd4
gageayk <64162749+gageayk@users.noreply.github.com>
2022-02-28 16:10:07 -0600

    fixed password

--- packages/api/src/orm.config.ts
+++ packages/api/src/orm.config.ts
@@ -53,7 +53,7 @@ export default {
   type: 'postgresql' as const,
   clientUrl: env.databaseUrl,
   user: env.databaseUser,
-  password: "Passowrd",
+  password: "Password",
   entities: [`${__dirname}/entities/*.js`],
   entitiesTs: [`${__dirname}/entities/*.ts`],
   forceUndefined: true,
